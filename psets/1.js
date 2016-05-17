3bf52f82ec9cc01a947bae3e2ac68c53d3516c78
Mark Opatow <zargosh@gmail.com>
2016-05-17 19:33:07 -0400

    solution for 1.js

--- psets/1.js
+++ psets/1.js
@@ -10,3 +10,12 @@ Forgot how old someone is? Calculate it!
 */
 
 // write your solution here...
+var currentYear = 2016;
+
+var birthYear = 1969;
+
+var age1 = (currentYear - birthYear);
+
+var age2 = (currentYear - birthYear) - 1;
+
+console.log("They are either " + age2+ " or " + age1+ ".");
