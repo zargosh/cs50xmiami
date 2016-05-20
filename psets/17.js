c4cb119db24c5029a65797261d2ebb8a58221d39
Mark Opatow <zargosh@gmail.com>
2016-05-20 16:13:25 -0400

    solution for 17.js

--- psets/17.js
+++ psets/17.js
@@ -10,3 +10,18 @@ Call that function for a few different scores and log the result to make sure it
 */
 
 // write your solution here...
+function pluralize(noun, number) {
+  if (noun == "geese" || noun == "deer" || noun == "sheep")
+    return console.log(number+ " " +noun + ".");
+  else if (number == 1)
+    return console.log(number+ " " +noun + ".");
+  else
+    return console.log(number+ " " +noun + "s.");
+}
+
+pluralize("cat", 5);
+pluralize("dog", 20);
+pluralize("sheep", 6);
+pluralize("geese", 3);
+pluralize("deer", 4);
+pluralize("aardvark", 1);
