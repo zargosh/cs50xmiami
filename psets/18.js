8fb91f4cf6a707acfd1468156274e372dac93a55
Mark Opatow <zargosh@gmail.com>
2016-05-20 16:34:41 -0400

    solution for 18.js

--- psets/18.js
+++ psets/18.js
@@ -7,3 +7,15 @@ The even/odd reporter
 */
 
 // write your solution here...
+// var i;
+//
+// for (i = 0; i < 20; i++) {
+//   return console.log(i+1); }
+
+  for(var i = 0; i < 20; i++){
+    if ((i+1) % 2 == 0)
+      console.log((i+1)+ " is even.");
+    else
+      console.log((i+1)+ " is odd."); }
+
+    // return;
