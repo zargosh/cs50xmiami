8204977d80ef1c60dc1c4b26af5f2ccca184468d
Mark Opatow <zargosh@gmail.com>
2016-05-20 16:51:23 -0400

    solution for 19.js

--- psets/19.js
+++ psets/19.js
@@ -7,5 +7,12 @@ Write a for loop that will iterate from 0 to 10.
 
 - Bonus: Use a nested for loop to show the tables for every multiplier from 1 to 10 (100 results total).
 */
-
+// console.log(i+ " * 9 = " +(i*9));
 // write your solution here...
+  for (var i = 0; i < 10; i++) {
+
+      for (var j = 0; j < 10; j++) {
+        console.log(i+ " * " +j + " = " +(i*j))
+      }
+      console.log("\n");
+  }
