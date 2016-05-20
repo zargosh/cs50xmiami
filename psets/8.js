a1b09ce0265337938eabab5f5f3bfad919bd3583
Mark Opatow <zargosh@gmail.com>
2016-05-20 09:20:29 -0400

    8.js with regex fixed

--- psets/8.js
+++ psets/8.js
@@ -17,33 +17,37 @@ For example:
 */
 
 
+
+
 function verbing(word) {
+
     var len = word.length;
+
     if (word.endsWith("ing"))
       return word.concat("ly");
+
     else if (len >= 3){
+
+      if (word.match(/[bcdfghjklmnpqrstvwxz][aeiou][bcdfghjklmnpqrstvz]$/))
+        return word.concat(word.charAt(word.length-1).concat("ing"));
+
       if (word.endsWith("e"))
         return word.substr(0, word.length-1).concat("ing");
+
       else if (word.endsWith("x"))
           return word.concat("ing");
+
       else if (word.endsWith("w"))
           return word.concat("ing");
-      /* there must be some way to combine the "x" and "w" endsWith statements,
-      but when I try to do it, it doesn't function properly
 
-      I want to check if the word is one syllable with the following pattern:
-      <consonant-vowel-consonant> and then double the last letter if it is.
-      It seems like it needs a RegExp statement that pattern matches the following,
-      but I can't get it to function properly
-        [a-z&&[^aeiou]][aeiou][a-z&&[^aeiou]]";
-        return word.concat(word.charAt(word.length-1).concat("ing")); */
       else
         return word.concat("ing");  }
+
     else return word;
 }
 
-var word = verbing("fox");
-var wordA = verbing("swimming");
+var word = verbing("dog");
+var wordA = verbing("pray");
 var wordB = verbing("go");
 
 console.log(word);
